05f439e696cdc9d70694112d059de0e5e8bbab85
Matias <mattiochoax@gmail.com>
2024-09-09 01:36:39 +0200

    Included the rest of HTTP methods and handlers - All API endpoints for Products DB is ready - Matias

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -14,6 +14,79 @@ export const getProducts = async (req: Request, res: Response) => {
 	}
 };
 
+export const getProductById = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+
+		const product = await Product.findByPk(id, {
+			attributes: { exclude: ["createdAt", "updatedAt"] },
+		});
+
+		if (!product) {
+			return res.status(404).json({
+				message: "Producto no encontrado",
+			});
+		}
+
+		res.json({
+			data: product,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const updateProduct = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+
+		const product = await Product.findByPk(id, {
+			attributes: { exclude: ["createdAt", "updatedAt"] },
+		});
+
+		if (!product) {
+			return res.status(404).json({
+				message: "Producto no encontrado",
+			});
+		}
+
+		// update
+		await product.update(req.body);
+		await product.save();
+
+		res.json({
+			data: product,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const updateAvailability = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+
+		const product = await Product.findByPk(id, {
+			attributes: { exclude: ["createdAt", "updatedAt"] },
+		});
+
+		if (!product) {
+			return res.status(404).json({
+				message: "Producto no encontrado",
+			});
+		}
+
+		// Actualizar la disponibilidad
+		product.available = !product.dataValues.available;
+		await product.save();
+		res.json({
+			data: product,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 export const createProduct = async (req: Request, res: Response) => {
 	// req.body va a leer lo que nosotros mandamos en el body de la peticion
 	// const product = new Product(req.body);
@@ -24,6 +97,30 @@ export const createProduct = async (req: Request, res: Response) => {
 
 		res.json({
 			data: product,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+	try {
+		const { id } = req.params;
+
+		const product = await Product.findByPk(id, {
+			attributes: { exclude: ["createdAt", "updatedAt"] },
+		});
+
+		if (!product) {
+			return res.status(404).json({
+				message: "Producto no encontrado",
+			});
+		}
+
+		await product.destroy();
+
+		res.json({
+			data: "Producto eliminado",
 		});
 	} catch (error) {
 		console.error(error);
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,45 @@
 import { Router } from "express";
-import { body } from "express-validator";
-import { createProduct, getProducts } from "./handlers/product";
+import { body, param } from "express-validator";
+import {
+	createProduct,
+	deleteProduct,
+	getProductById,
+	getProducts,
+	updateAvailability,
+	updateProduct,
+} from "./handlers/product";
 import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
 // Routing
+//GET
 router.get("/", getProducts);
 
+router.get(
+	"/:id",
+	param("id").isInt().withMessage("El id debe ser un numero"),
+	handleInputErrors,
+	getProductById
+);
+
+// PUT
+router.put(
+	"/:id",
+	param("id").isInt().withMessage("El id debe ser un numero"),
+	handleInputErrors,
+	updateProduct
+);
+
+// PATCH
+router.patch(
+	"/:id",
+	param("id").isInt().withMessage("El id debe ser un numero"),
+	handleInputErrors,
+	updateAvailability
+);
+
+// POST
 router.post(
 	"/",
 	/* Validacion */
@@ -19,6 +51,14 @@ router.post(
 		.custom((value) => value > 0),
 	handleInputErrors,
 	createProduct
+);
+
+// DELETE
+router.delete(
+	"/:id",
+	param("id").isInt().withMessage("El id debe ser un numero"),
+	handleInputErrors,
+	deleteProduct
 );
 
 export default router;
