89c8cebb2ec14d62fbaf036aa4af4a0ce86bca46
Matias <mattiochoax@gmail.com>
2024-09-18 00:31:53 +0200

    Changes applied for CORS and morgan for logging requests - Matias

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import router from "./router";
 import colors from "colors";
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 import swaggerUi, { SwaggerUiOptions } from "swagger-ui-express";
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger";
 import db from "./config/db";
@@ -23,8 +25,25 @@ connectDB();
 // Instancia de express
 const server = express();
 
+// Permitir cors
+const corsOptions: CorsOptions = {
+	// quien envia la peticion
+	origin: function (origin, callback) {
+		if (origin === process.env.FRONTEND_URL) {
+			callback(null, true); // primer parametro es el error, segundo es si permitimos la conexion
+		} else {
+			callback(new Error("No permitido por CORS"));
+		}
+	},
+};
+
+server.use(cors(corsOptions));
+
 // Leer datos de formularios
 server.use(express.json());
+
+// Morgan
+server.use(morgan("dev"));
 
 server.use("/api/products", router);
 
